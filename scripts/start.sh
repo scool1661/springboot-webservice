a64e79c5446d1c124726f28d0c12d177023e8da1
HW <HW@DESKTOP-75IC3JL>
2020-05-11 23:53:11 +0900

    무

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,8 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot-webservice
 
+cd $REPOSITORY
+
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.war $REPOSITORY/"
 cp $REPOSITORY/zip/*.war $REPOSITORY/
@@ -14,8 +16,10 @@ cp $REPOSITORY/zip/*.war $REPOSITORY/
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep war | tail -n 1)
 IDLE_PROFILE=$(find_idle_profile)
 
+
+
 echo "> 새 애플리케이션($JAR_NAME) 배포"
-cd $REPOSITORY
+
 nohup java -jar \
 	-Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
 	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
