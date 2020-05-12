35e66e6d1c4ad92c8a6d6b88f477ea066683404b
HW <HW@DESKTOP-75IC3JL>
2020-05-12 10:38:35 +0900

    sleep

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,13 +3,13 @@
 # 쉬고 있는 프로파일 찾기
 function find_idle_profile()
 {
-	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/profile)
+	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 	
 	if [ ${RESPONSE_CODE} -ge 400 ]
  	then
- 		CURRENT_PROFILE=real2
+ 		CURRENT_PROFILE=real2 # 스프링부트 애플리케이션이 실행되고 있지 않은 경우
  	else
- 		CURRENT_PROFILE=real1
+ 		CURRENT_PROFILE=$(curl -s http://localhost/profile) # real1 또는 real2
  	fi
  	
  	if [ ${CURRENT_PROFILE} == real1 ]
